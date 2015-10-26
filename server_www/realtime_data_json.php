c4b28db265baaa0caa294a4d78e7b61efbce622c
krishinsh@gmail.com <krishinsh@gmail.com>
2015-10-26 22:22:41 +0200

    sad

--- server_www/realtime_data_json.php
+++ server_www/realtime_data_json.php
@@ -17,5 +17,24 @@ $array_of_readings =json_decode($the_data);
 
 var_dump($array_of_readings);
 
+foreach ($array_of_readings as $key -> $value) 
+	{
+		$sensor_id = $key;
+		$sensor_array['sensor_name'] = 'name placeholder';
+		$sensor_array['value'] = $value;
+		
+		$output_new[$sensor_id] = $sensor_array;
+		
+	}
+
+
 print ("<hr>");
+
+
+print json_encode($output_new);
+
+print ("<hr>");
+
+var_dump($output_new);
+
 ?> 
