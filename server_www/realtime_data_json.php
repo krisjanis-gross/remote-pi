15f395df40f04109f8998f2e1d6c1d4741e021f7
krishinsh@gmail.com <krishinsh@gmail.com>
2015-10-26 22:24:53 +0200

    asd

--- server_www/realtime_data_json.php
+++ server_www/realtime_data_json.php
@@ -1,5 +1,8 @@
 <?php 
 
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
 header('Cache-Control: no-store, no-cache, must-revalidate');
 
 include("read_thermometers.php");
@@ -17,7 +20,7 @@ $array_of_readings =json_decode($the_data);
 
 var_dump($array_of_readings);
 
-foreach ($array_of_readings as $key -> $value) 
+foreach ($array_of_readings as $key => $value) 
 	{
 		$sensor_id = $key;
 		$sensor_array['sensor_name'] = 'name placeholder';
