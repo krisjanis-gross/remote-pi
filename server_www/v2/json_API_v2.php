a63e8c71075312957447aee06dd18448ba6c84ab
Krisjanis Gross <krishinsh@gmail.com>
2018-06-29 13:28:49 +0300

    123

--- server_www/v2/json_API_v2.php
+++ server_www/v2/json_API_v2.php
@@ -38,8 +38,22 @@ switch ($request_action) {
 }
 
 function return_data_to_client($return_data) {
+  // return result
+//http://stackoverflow.com/questions/18382740/cors-not-working-php
+	if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 0');    // cache for 0 day
+    }
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+        exit(0);
+    }
   print json_encode($return_data);
-
 }
 
 
