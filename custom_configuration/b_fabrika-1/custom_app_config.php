43dd1fccab7df3ac2b9f9a3f1905df1bce53e368
krisjanis-gross <krishinsh@gmail.com>
2021-07-12 11:39:03 +0300

    1
    new custom config

---
+++ custom_configuration/b_fabrika-1/custom_app_config.php
@@ -0,0 +1,28 @@
+<?php
+// app config.
+
+// token used for API calls
+$config_API_KEY = "new-key";
+
+// other parameters
+$sensor_reading_db_log_interval = 60 ; // seconds
+
+// sensor data save parameters
+$allDataSaveHours = 3;
+$allDataSaveIntervalSeconds = 1 * 60; // 1 minute
+
+$midTermSaveDays = 90;
+$midTermSaveIntervalSeconds = 10 * 60; //10 minutes
+
+$longTermSaveDays = 365;  // all records older than this will be deleted.
+$longTermSaveIntervalSeconds = 60 * 60; // 1 hour
+
+
+// monitoring parameters
+$montiror_URL = "https://helloworld-evonpsrdjq-ew.a.run.app/";
+$monitor_enabled = true;
+$monitor_API_key = "rb82975298457hk";
+$monitor_node_ID = 22222;
+$monitor_node_NAME = "betras-fabrika-1";
+
+?>
---
+++ custom_configuration/b_fabrika-1/custom_read_sensor_data.php
@@ -0,0 +1,72 @@
+<?php
+
+function 	read_sensor_data_custom ()  {
+	// read ds18b20 sensor data
+	$DS18B20_reading = exec ("sudo python /home/pi/remote_pi/read_DS18B20_thermometers.py");
+	if ($DS18B20_reading <> "")	{
+    $DS18B20_reading = "{" . $DS18B20_reading . "}";
+		/// must be tested.
+		$DS18B20_readings = json_decode($DS18B20_reading);
+    //error_log ($DS18B20_reading);
+    
+    foreach ( $DS18B20_readings as $key => $value) {
+    
+   // error_log ("kkkkkkkkkkkkkkkkkey $key");
+   // error_log ("vvvvvvvvvvvvvvvalue $value");
+    // chech if value is in "reasonable" range. e.g. not an error.
+    if ($value > -50 && $value < 200){
+          add_sensor_reading($key,$value);
+      }
+    }
+		// foreach ...
+	  // add_sensor_reading($sensor_id,$readingvalue);
+
+	}
+	//var_dump($all_data);
+
+// read DHT11 sensor data
+	// DHT11 SENSOR DATA
+	$dht11_data = exec ("python3 /home/pi/remote_pi/read_DHT_sensor.py");
+	if ($dht11_data != "error") {
+	//		$all_data = str_replace ("}", "" , $all_data );
+			$dht11_readings = json_decode($dht11_data);
+    //  error_log ($dht11_data);
+      foreach ( $dht11_readings as $key => $value) {
+    
+         // error_log ("kkkkkkkkkkkkkkkkkey $key");
+         // error_log ("vvvvvvvvvvvvvvvalue $value");
+          // chech if value is in "reasonable" range. e.g. not an error.
+                if ($value > -50 && $value < 200){
+                      add_sensor_reading($key,$value);
+                  }
+          }
+      
+	}
+ 
+ 
+  // calculate gaisa mitruma pakaape and add as a reading
+ require_once("functions_triggers.php");
+ $FI = get_sensor_reading ('dht_humidity') ;
+ $t = get_sensor_reading ('28-0115a8683aff') ;
+ 
+ if (is_numeric($FI) AND is_numeric($t) ){
+		$T = 273 + $t ;
+		$P_ws = (exp(77.345 + 0.0057 * $T - 7235/$T)) / pow ($T,8.2);
+		$P_w = ($FI/100) * $P_ws;
+		$P_a = 101325;
+		$X_result = (0.628 * ($P_w / ($P_a - $P_w))) ;
+		$X_result = round ($X_result * 10000 , 2);
+		//error_log ("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% T = " . $T . "  P_ws = ". $P_ws. "  FI = " . $FI . "  P_w = " . $P_w. "  P_a = ". $P_a. "  X_result = ". $X_result );
+   add_sensor_reading("gaisa_mitruma_pakaape",$X_result);
+	}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
+
+
+?>
