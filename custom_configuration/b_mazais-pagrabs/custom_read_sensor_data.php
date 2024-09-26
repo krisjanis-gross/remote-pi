c5ca66eefd2051a476eddb5ba3b0fc54affa79e4
krisjanis-gross <krishinsh@gmail.com>
2024-09-26 20:20:54 +0300

    a
    a

--- custom_configuration/b_mazais-pagrabs/custom_read_sensor_data.php
+++ custom_configuration/b_mazais-pagrabs/custom_read_sensor_data.php
@@ -3,7 +3,7 @@
 function 	read_sensor_data_custom ()  {
 	// read ds18b20 sensor data
 
- $repeat_counter = 0; $max_repeat_count = 5;
+ $repeat_counter = 0; $max_repeat_count = 50;
  while ( $repeat_counter < $max_repeat_count)
      {
        $DS18B20_reading = exec ("sudo python /home/pi/remote_pi/read_DS18B20_thermometers.py");
@@ -17,8 +17,10 @@ function 	read_sensor_data_custom ()  {
 
             // chech if value is in "reasonable" range. e.g. not an error.
             if ($value > -50 && $value < 200){
+					if (validate_temp_reading ($key,$value)) {
                     $value = round($value, 1);
                     add_sensor_reading($key,$value);
+					}
                 }
             }
          break;
@@ -34,4 +36,41 @@ function 	read_sensor_data_custom ()  {
 }
 
 
+function validate_temp_reading ($key,$value,$valid_delta = 1,$max_fail_limit = 10){
+	// get previous reading data from apc (if exists)
+	$previous_value = apcu_fetch('previous_value' . $key , $previous_value);
+	$fail_count  = apcu_fetch('fail_count' . $key , $fail_count);
+
+	if (!$previous_value) {
+		apcu_store('previous_value' . $key, $value);
+		apcu_store('fail_count' . $key, 0);
+		$result = true;
+	}
+	else {
+		// check delta value.
+		$delta = abs($previous_value - $value);
+		if ($delta <= $valid_delta) {
+			// OK
+			apcu_store('previous_value' . $key, $value);
+		    apcu_store('fail_count' . $key, 0);
+			$result = true;
+		}
+		else {
+			// not OK
+			if ($fail_count <= $max_fail_limit) {
+				apcu_store('fail_count' . $key, $fail_count + 1 );
+				$result = false;
+				error_log ("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@reading value check FAILED id = $key :  previous = $previous_value current =$value  delta $delta   ");
+			}
+			else {
+				// maximum fail count reached. accept the value as 'ok'
+				apcu_store('previous_value' . $key, $value);
+				apcu_store('fail_count' . $key, 0);
+				$result = true;
+			}
+		}
+	}
+//	error_log ("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@reading value check id = $key :  previous = $previous_value current =$value  delta $delta  result =	$result ");
+	return $result;
+}
 ?>
--- custom_configuration/b_mazais-pagrabs/custom_trigger_script.php
+++ custom_configuration/b_mazais-pagrabs/custom_trigger_script.php
@@ -51,40 +51,42 @@ if ($trigger_id == 1 && $command == 0) {
 
 
 function process_trigger_1()
- 
+
  {
  $pagrabs_T = get_sensor_reading ('28-0316224c1dff') ;
  $aara_T = get_sensor_reading ('28-0415a86941ff') ;
  $merka_T = get_parameter (1);	if (!is_numeric($merka_T)) $merka_T = 5;
  $delta_T_merka = get_parameter (2);	if (!is_numeric($delta_T_merka)) $delta_T_merka = 1;
  $delta_T_pagrabs_aara = get_parameter (3);	if (!is_numeric($delta_T_pagrabs_aara)) $delta_T_pagrabs_aara = 1;
- 
- error_log ("@@@@@@@@@@@@@@@@@@Trigger parameters: pagrabs_T $pagrabs_T   aara_T   $aara_T  merka_T $merka_T   delta_T_merka  $delta_T_merka   delta_T_pagrabs_aara  $delta_T_pagrabs_aara");
- 
- 
+ $delta_T_pagrabs_aara_IZSL = get_parameter (4);	if (!is_numeric($delta_T_pagrabs_aara_IZSL)) $delta_T_pagrabs_aara_IZSL = 0.5;
+
+
+
   if ( !is_null($pagrabs_T) AND  !is_null($aara_T) ) {
-      if  ($pagrabs_T >= ($merka_T + $_delta)) { // vai vajag dzeset? 
-            if  ($pagrabs_T >= ($aara_T + $_delta)) { // vai var dzeset?
+      if  ($pagrabs_T >= ($merka_T + $delta_T_merka)) { // vai vajag dzeset?
+            if  ($pagrabs_T >= ($aara_T + $delta_T_pagrabs_aara)) { // vai var dzeset?
                     $ventolators_action  = 1; // on
               }
-              elseif  ($pagrabs_T <= ($aara_T - $_delta)) {
+              elseif  ($pagrabs_T <= ($aara_T + $delta_T_pagrabs_aara_IZSL)) {
                     $ventolators_action  = 0; // off
               }
       }
-      elseif  ($pagrabs_T <= ($merka_T - $_delta)) {
+      elseif  ($pagrabs_T <= ($merka_T - $delta_T_merka)) {
             $ventolators_action  = 0; // off
       }
   }
-  
+
   if (isset ($ventolators_action )) {
       $previous_pin_status = get_pin_status (8);
       set_pin (8, $ventolators_action,false);
-      if ($previous_pin_status <> $ventolators_action){  
+      if ($previous_pin_status <> $ventolators_action){
               add_sensor_reading("ventilators_relejs_8",$ventolators_action * 4);
               global $trigger_log_data;
      	        $trigger_log_data = true;
           }
     }
+
+    // error_log ("@@@@@@@@@@@@@@@@@@Trigger parameters: pagrabs_T $pagrabs_T   aara_T   $aara_T  merka_T $merka_T   delta_T_merka  $delta_T_merka   delta_T_pagrabs_aara  $delta_T_pagrabs_aara   ventolators_action   $ventolators_action");
 
 
  }
