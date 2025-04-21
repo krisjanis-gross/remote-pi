6f8b0541e62e807690229538a4b9257c46e9b320
Krišjānis Gross <krishinsh@gmail.com>
2025-04-21 20:04:32 +0300

    ha

--- server_www/json_ha.php
+++ server_www/json_ha.php
@@ -21,7 +21,7 @@ die();
 
 // Takes raw data from the request
 $json = file_get_contents('php://input');
-error_log($json);
+//error_log($json);
 // Converts it into a PHP object
 $data = json_decode($json);
 
@@ -41,8 +41,17 @@ $active = null;
 
 
 require_once("functions_gpio_control.php");
-$pin_status = get_pin_status ($pin_id ) ;
 
+
+
+if ($active == true) set_pin ($pin_id, true);
+if ($active == false) set_pin ($pin_id, false);
+
+
+
+
+// return result
+$pin_status = get_pin_status ($pin_id ) ;
 if ($pin_status) print ('{"is_active": "true"}');
 else print ('{"is_active": "false"}');
 
