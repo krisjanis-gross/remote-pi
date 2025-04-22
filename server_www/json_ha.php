9561320ef62984c3e363272c076fc629e13ac58e
Krišjānis Gross <krishinsh@gmail.com>
2025-04-22 07:57:58 +0300

    ha

--- server_www/json_ha.php
+++ server_www/json_ha.php
@@ -11,7 +11,6 @@ else // some default config
 }
 
 
-
 if (isset($_GET["pin_id"])) {
 $pin_id = htmlspecialchars($_GET["pin_id"]);
 }
@@ -39,31 +38,31 @@ else {
 $active = null;
 }
 
+//error_log("json_ha called. pin_id = $pin_id active = $active  API_key = $request_API_key ");
+
+// check API key.
+if ($request_API_key <> $config_API_KEY ) {
+	echo ("!!!!!!!!!!!!!!API KEY not correct!" . $request_API_key ) ;
+	error_log ("!!!!!!!!!!!!!!API KEY not correct!" . $request_API_key ) ;
+	die();
+}
+
 require_once("db_app_data_functions.php");
 require_once("functions_gpio_control.php");
 
 
-
 if ($active == "true") process_gpio2 ($pin_id, 1);
 if ($active == "false") process_gpio2 ($pin_id, 0);
-
-
-
 
 // return result
 $pin_status = get_pin_status ($pin_id ) ;
 if ($pin_status) print ('{"is_active": "true"}');
 else print ('{"is_active": "false"}');
 
-// check API key.
-//if ($request_API_key <> $config_API_KEY ) {
-//	echo ("!!!!!!!!!!!!!!API KEY not correct!" . $request_API_key ) ;
-//	error_log ("!!!!!!!!!!!!!!API KEY not correct!" . $request_API_key ) ;
-//	die();
-//}
 
 
-error_log("json_ha called. pin_id = $pin_id active = $active ");
+
+
 
 
 
